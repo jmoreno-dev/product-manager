3b136d827dda8abdb470c30f62b6ad76b9059391
jmoreno-dev <ytanonim12@gmail.com>
2026-01-07 17:04:13 +0100

    created product service

---
+++ backend/src/main/java/com/jmoreno_dev/productmanager/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.jmoreno_dev.productmanager.service;
+
+import com.jmoreno_dev.productmanager.entity.Product;
+import com.jmoreno_dev.productmanager.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveOrUpdateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public boolean deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) return false;
+        productRepository.deleteById(id);
+        return true;
+    }
+}
