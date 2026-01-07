4c0ec1c6ed15c4a13828ea458b7db65390af6ee4
jmoreno-dev <ytanonim12@gmail.com>
2026-01-07 16:54:01 +0100

    cleared app.html and app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProductListComponent } from './components/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProductListComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App { }
