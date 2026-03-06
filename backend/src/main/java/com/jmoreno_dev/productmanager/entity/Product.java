4cc9c228f86ce60ba78ccb798d0d9180169fb11c
jmoreno-dev <ytanonim12@gmail.com>
2026-03-06 18:04:35 +0100

    added constructor to Product

--- backend/src/main/java/com/jmoreno_dev/productmanager/entity/Product.java
+++ backend/src/main/java/com/jmoreno_dev/productmanager/entity/Product.java
@@ -23,4 +23,15 @@ public class Product {
 
     @Column (name = "quantity", nullable = false)
     private Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
