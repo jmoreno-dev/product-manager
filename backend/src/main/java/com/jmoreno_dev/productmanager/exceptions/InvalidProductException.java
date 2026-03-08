17549b218b275eccf049ceb4c0a8479d2cbb57cb
jmoreno-dev <ytanonim12@gmail.com>
2026-03-08 15:50:43 +0100

    modified InvalidProductException

--- backend/src/main/java/com/jmoreno_dev/productmanager/exceptions/InvalidProductException.java
+++ backend/src/main/java/com/jmoreno_dev/productmanager/exceptions/InvalidProductException.java
@@ -1,6 +1,6 @@
 package com.jmoreno_dev.productmanager.exceptions;
 
-public class InvalidProductException extends RuntimeException {
+public class InvalidProductException extends Exception {
     public InvalidProductException(String message) {
         super(message);
     }
