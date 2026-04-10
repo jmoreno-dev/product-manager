19807a018bf60c85d7e2595bace1e3c6eb99f48d
jmoreno-dev <ytanonim12@gmail.com>
2026-04-10 12:44:08 +0200

    back to origin deleting mapper and dtos

--- backend/src/main/java/com/jmoreno_dev/productmanager/dto/CreateProductRequest.java
+++
@@ -1,9 +0,0 @@
-package com.jmoreno_dev.productmanager.dto;
-
-public record CreateProductRequest(
-        String name,
-        String description,
-        Double price,
-        Integer quantity
-) {
-}
--- backend/src/main/java/com/jmoreno_dev/productmanager/dto/ProductDTO.java
+++
@@ -1,9 +0,0 @@
-package com.jmoreno_dev.productmanager.dto;
-
-public record ProductDTO(
-        Long id,
-        String name,
-        String description,
-        Double price,
-        Integer quantity
-) { }
