e2236d22cd376ee5defa8387b93f9f5250edb0fe
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-29 14:30:27 +0200

    Remove unnecessary SpringBootTest annotation from ProductmanagerApplicationTests

--- backend/src/test/java/com/jmoreno_dev/productmanager/ProductmanagerApplicationTests.java
+++ backend/src/test/java/com/jmoreno_dev/productmanager/ProductmanagerApplicationTests.java
@@ -1,9 +1,6 @@
 package com.jmoreno_dev.productmanager;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ProductmanagerApplicationTests {
 
 	@Test
